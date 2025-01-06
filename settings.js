2a5bc53583b1321f897004b1a4e39f4a42bbd693
Allhailnuel <chimbuezeugwu@gmail.com>
2025-01-06 01:01:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const version = require("@whiskeysockets/baileys/package.json").version
 //Input number for pair code
-global.pairingNumber = "2349019529423"
+global.pairingNumber = "2347068873198"
 
-global.botNumber = ["2349019529423"]
+global.botNumber = ["2347068873198"]
 global.domain = "https://.nobodysey.me"
 global.apikey = "ptla_DfGlmoY7jCzmkNUgyK925FPVE08Kr4mxwLMep"
 global.capikey = "ptlc_GTm42E3qIwbLW3K9w3htljUvhGvnG71hoPTqE"
@@ -20,9 +20,9 @@ global.autoBio = true
 global.antiSpam = true
 global.sign = "☃️❄️"
 // ☞ ➻ ➽ ☛ 〆 
-global.bank = "Opay"
-global.bankname = "BLUE"
-global.accnumber = "7041039367"
+global.bank = "PALMPAY"
+global.bankname = "NUEL"
+global.accnumber = "7068873198"
 global.language = "en"
 global.sessionName = "session"
 global.pairingCode = true 
